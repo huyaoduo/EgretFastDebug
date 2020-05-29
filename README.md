7cd8274845117c50e3db5d34e23ea8f0fd73131b
胡耀铎 <yaoduo.hu@rjoygame.com>
2020-05-29 13:56:28 +0800

    0.0.5

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Fast Debug for Egret engine
 
-## install
+## Install
 
     直接在vscode的插件商店中搜索"egret fast debug"安装
 
     或本地安装:cmd+shift+p 选择使用vsix安装
 
-## introduce
+## Introduce
 
     此插件将在vscode中,打开白鹭项目的ts文件时被激活,开始监听ts文件更改
     
@@ -20,34 +20,39 @@
 
 ## Features
 
-   use vscode fileWatcher to watch .ts file if changed
+    Use vscode's fileWatcher to watch .ts files if changed
 
-   使用vscode监测工作区白鹭项目的ts文件改动
+    使用vscode监测工作区白鹭项目的ts文件改动
 
 ## Dependencies
 
-    nodejs,typescript
+    Confirm your node and typescript version,use "node -v" and "tsc -v".
 
-    To confirm your node and typescript version,use "node -v" and "tsc -v".
+    确保环境中已安装node
+    0.0.2版本内置typescript依赖,无需单独安装了
 
-    确保环境中已安装node,typescript
-    
-    --add:0.0.2版本内置typescript依赖,无需单独安装了
+## History
 
-### 0.0.1
+### 0.0.5
 
-Init tool
+    新增使用任务队列进行处理文件,多个文件同时变动将不会导致manifest.json出错了
 
-初始化
+### 0.0.4
 
-### 0.0.2
-
-Add a necessary dependency into the package
-
-添加了一个必要依赖至包体中
+    在egret wing中,移除了弹框提示,改为输出提示
+    现在提示中会包括具体文件名
 
 ### 0.0.3
 
-support egret wing3
+    Support egret wing3
+    支持egret wing3编辑器
 
-支持egret wing3编辑器
+### 0.0.2
+
+    Add a necessary dependency into this package
+    添加了一个必要依赖至包体中
+
+### 0.0.1
+
+    Init tool
+    初始化
--- extension.js
+++ extension.js
@@ -6,13 +6,16 @@ const path = require('path');
 const exec = require('child_process');
 const jsonfile = require('jsonfile');
 var isWing = false;
+let rootPath = '';
+var workLine = [];
+var isWorking = false;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
 function activate() {
-	let rootPath = '';
+	//let rootPath = '';
 	let wing = null;
 	if (vscode.workspace.workspaceFolders) {
 		rootPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
@@ -21,28 +24,41 @@ function activate() {
 		wing = require('wing');
 		rootPath = vscode.workspace.rootPath;
 	}
-	let head = isWing?wing:vscode;
+	let head = isWing ? wing : vscode;
 	if (rootPath) {
 		let list = fs.readdirSync(rootPath);
 		if (list.indexOf('.wing') != -1) {
 			console.log(rootPath);
 			let watcher = head.workspace.createFileSystemWatcher(rootPath + '/src/**/*.ts');
 			watcher.onDidChange((url) => {
-				console.log('onDidChange');
+				//console.log('onDidChange');
 				if (url.fsPath.indexOf('.ts') != -1) {
+					let fileName = path.basename(url.fsPath);
 					let callback = function () {
-						head.window.showInformationMessage('Egret Fast Debug: 编译完成');
+						if (isWing) {
+							echo(1,fileName);
+						} else {
+							head.window.showInformationMessage(`Egret Fast Debug: ${fileName}编译完成`);
+						}
 					}
-					changeFile(rootPath, url.fsPath, callback);
+
+					//changeFile(rootPath, url.fsPath, callback);
+					addWork(url.fsPath,1,callback)
 				}
 			});
 			watcher.onDidCreate((url) => {
 				console.log('onDidCreate');
 				if (url.fsPath.indexOf('.ts') != -1) {
 					let callback = function () {
-						head.window.showInformationMessage('Egret Fast Debug: 编译完成');
+						let fileName = path.basename(url.fsPath);
+						if (isWing) {
+							echo(1, fileName);
+						} else {
+							head.window.showInformationMessage(`Egret Fast Debug: ${fileName}编译完成`);
+						}
 					}
-					createFile(rootPath, url.fsPath, callback);
+					//createFile(rootPath, url.fsPath, callback);
+					addWork(url.fsPath,2,callback)
 				}
 
 				// shell(url.fsPath,()=>{
@@ -54,13 +70,23 @@ function activate() {
 				console.log('onDidDelete');
 				if (url.fsPath.indexOf('.ts') != -1) {
 					let callback = function () {
-						head.window.showInformationMessage('Egret Fast Debug: 删除成功');
+						let fileName = path.basename(url.fsPath);
+						if (isWing) {
+							echo(2,fileName);
+						} else {
+							head.window.showInformationMessage(`Egret Fast Debug: ${fileName}删除成功`);
+						}
 					}
-					delFile(rootPath, url.fsPath, callback);
+					//delFile(rootPath, url.fsPath, callback);
+					addWork(url.fsPath,3,callback)
 				}
 
 			});
-			head.window.showInformationMessage('Egret Fast Debug: 加载成功');
+			if (isWing) {
+				echo(3);
+			} else {
+				head.window.showInformationMessage('Egret Fast Debug: 插件加载成功');
+			}
 		}
 
 
@@ -74,13 +100,28 @@ function deactivate() {
 
 }
 
+function echo(type, fileUrl) {
+	let wing = require('wing');
+	let out = wing.window.createOutputChannel("EgretFastDebug");
+	out.show();
+	if (type == 1) {
+		out.appendLine(`EgretFastDebug：${fileUrl}编译成功`);
+	} else if (type == 2) {
+		out.appendLine(`EgretFastDebug：${fileUrl}删除成功`);
+	} else {
+		out.appendLine("EgretFastDebug：加插件加载成功");
+	}
+
+}
+
+
 function shell(rootPath, fsPath, cb) {
 	let outPath = path.dirname(fsPath).replace(path.join(rootPath, 'src'), path.join(rootPath, 'bin-debug'));
 	let tsc = path.join(__dirname, './node_modules/typescript/bin/tsc');
-	if(isWing){
-		tsc = tsc.replace('Application Support','Application\\ Support');
+	if (isWing) {
+		tsc = tsc.replace('Application Support', 'Application\\ Support');
 	}
-	console.log(tsc);
+	//console.log(tsc);
 	exec.exec(`${tsc} --sourceMap --outDir ${outPath} ${fsPath}`, function () {
 		if (cb) cb();
 	})
@@ -94,8 +135,14 @@ function changeFile(rootPath, fsPath, cb) {
 
 function createFile(rootPath, fsPath, cb) {
 	let list = fs.readdirSync(rootPath);
+	let count = 0;
 	if (list.indexOf('manifest.json') != -1) {
-		shell(rootPath, fsPath, cb);
+		shell(rootPath, fsPath, ()=>{
+			count += 1;
+			if(count > 1){
+				cb()
+			}
+		});
 		let manifestPath = path.join(rootPath, 'manifest.json');
 		let temp = fsPath.replace(path.join(rootPath, 'src'), 'bin-debug');
 		let newContent = temp.replace('.ts', '.js');
@@ -105,6 +152,10 @@ function createFile(rootPath, fsPath, cb) {
 			data.game.push(newContent);
 			jsonfile.writeFile(path.join(rootPath, 'manifest.json'), data, { spaces: 2 }, function (err) {
 				if (err) throw err;
+				count += 1;
+				if(count > 1){
+					cb()
+				}
 			})
 		})
 	} else {
@@ -133,6 +184,43 @@ function delFile(rootPath, fsPath, cb) {
 		vscode.window.showInformationMessage('未找到manifest.json,请先在wing中编译项目');
 	}
 
+}
+
+function doWork(){
+	if(isWorking) return;
+	let nowWork = workLine.shift();
+	let fspath = nowWork.fspath;
+	let type = nowWork.type;
+	let cb = nowWork.cb;
+	console.log('nowWork' , path.basename(fspath));
+	
+	let callBack = function(){
+		console.log('done');
+		if(cb){
+			cb();
+		}
+		isWorking = false;
+		if(workLine.length > 0){
+			doWork()
+		}
+	};
+	isWorking = true;
+	if(type == 1){
+		changeFile(rootPath, fspath, callBack);
+	}else if(type == 2){
+		createFile(rootPath, fspath, callBack);
+	}else if(type == 3){
+		delFile(rootPath, fspath, callBack);
+	}
+
+}
+
+function addWork(fspath,type,cb){
+	if(!workLine){
+		workLine = [];
+	}
+	workLine.push({fspath:fspath,type:type,cb:cb});
+	doWork();
 }
 
 module.exports = {
